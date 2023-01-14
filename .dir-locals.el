a206abd40708339c4b694fac1e41a572da5ed0e0
Entroperiance.J <bmsac0001@gmail.com>
2023-01-15 00:11:03 +0800

    [ehome] bump to 0.9.99991 for firefox

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((prog-mode . ((tab-width . 4)
+               (indent-tabs-mode . t)
+               (entropy/emacs-inhibit-simple-whitespace-clean . nil))))

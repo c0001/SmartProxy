91e034632341f169aee25170a193f397e9ae25e9
Entroperiance.J <bmsac0001@gmail.com>
2023-01-29 09:42:05 +0800

    Make package.json same formating as `origin/master'

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((prog-mode . ((tab-width . 4)
-               (indent-tabs-mode . t)
-               (entropy/emacs-inhibit-simple-whitespace-clean . nil))))
+			   (indent-tabs-mode . t)
+			   (entropy/emacs-inhibit-simple-whitespace-clean . t))))

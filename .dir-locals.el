bb5a9d1122f30e1019bf3fda55acc3205317d3d4
Entroperiance.J <bmsac0001@gmail.com>
2023-02-08 00:13:00 +0800

    [ehome] code style: Reduce emacs tab-width for more compact visual

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((prog-mode . ((tab-width . 4)
+((prog-mode . ((tab-width . 2)
 			   (indent-tabs-mode . t)
 			   (entropy/emacs-inhibit-simple-whitespace-clean . t))))

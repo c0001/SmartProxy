06083942e3a6125f2cec5c055cad117984c2bda9
Entroperiance.J <bmsac0001@gmail.com>
2023-03-28 17:22:56 +0800

    Use .editorconfig instead of .dir-locals

--- .dir-locals.el
+++
@@ -1,6 +0,0 @@
-;;; Directory Local Variables            -*- no-byte-compile: t -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((prog-mode . ((tab-width . 2)
-			   (indent-tabs-mode . t)
-			   (entropy/emacs-inhibit-simple-whitespace-clean . t))))
